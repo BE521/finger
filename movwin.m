4b18f5e865eda995bc4171e1f2052f8ffd12278f
jiawei-chen <cjiawei@seas.upenn.edu>
2017-04-26 19:17:17 -0400

    moving windows

---
+++ movwin.m
@@ -0,0 +1,19 @@
+%%
+% data - a vector of original data
+% winLen - length of each window
+% winDisp - overlapped length of each window
+% type - 1.left align / 2. middle align / 3. right align
+% return value - an matrix of windows, 
+function out=movwin(data, winlen, overlap, type)
+n=floor((length(data)-overlap)/(winlen-overlap));
+switch type
+    case 1
+        init=0;
+    case 2
+        init=round((length(data)-(n-1)*(winlen-overlap)-winlen)/2);
+    case 3
+        init=round(length(data)-(n-1)*(winlen-overlap)-winlen);
+end
+sample=(0:n-1)*(winlen-overlap)+1+init;
+sample=repmat(sample,winlen,1)+repmat(((1:winlen)-1)',1,n);
+out=data(sample);
